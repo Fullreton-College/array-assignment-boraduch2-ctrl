2d213acbb3fb128a283f1c3ec29d7be9b16ce174
Duch, Bora <boraduch2@gmail.com>
2026-03-19 19:12:44 -0700

    Completed ArraysAssignment2 counting perfect scores

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -12,9 +12,19 @@ int main() {
 
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
 
-    // Store scores and keep track of how many are entered
-    // Report perfect scores using the helper function
-  // COMPLETE THE FUNCTION BELOW
+    while (count < 20)
+    {
+        cin >> input;
+
+        if (input == -1)
+            break;
+
+        if (input >= 0 && input <= 100)
+        {
+            scores[count] = input;
+            count++;
+        }
+    }
   int perfectOnes = countPerfect(scores, count);
     
     cout << "\nYou entered " << count << " scores." << endl;
@@ -26,6 +36,13 @@ int main() {
 // Value-returning function to count scores of 100
 int countPerfect(const int scores[], int size) {
     int totalPerfect = 0;
-    //COMPLETE THE CODE 
+  
+    for (int i = 0; i < size; i++)
+    {
+        if (scores[i] == 100)
+        {
+            totalPerfect++;
+        }
+    } 
     return totalPerfect;
 }
